442668e68d6132e727ff4afc50b2b5182f844f54
Mirzobek Xaitov <mirzobekxaitov@gmail.com>
2025-08-15 16:52:58 +0500

    15-avgust -test_check_box.py

--- pages/check_box.py
+++ pages/check_box.py
@@ -1,34 +1,130 @@
 from selenium.webdriver.common.by import By
 from base.base_page import BasePage
 
+from selenium.webdriver.common.by import By
+from base.base_page import BasePage
+
+
 class Check_box_page(BasePage):
+    main_toggle_button = (By.CSS_SELECTOR, "button.rct-collapse.rct-collapse-btn")
+    # Har bir papka/fayl oldidagi o'chirish/yoqish tugmasi
+    home_checkbox = (By.CSS_SELECTOR, "label[for='tree-node-home'] span.rct-checkbox")
+    desktop_checkbox = (By.CSS_SELECTOR, "label[for='tree-node-desktop'] span.rct-checkbox")
+    documents_checkbox = (By.CSS_SELECTOR, "label[for='tree-node-documents'] span.rct-checkbox")
+    downloads_checkbox = (By.CSS_SELECTOR, "label[for='tree-node-downloads'] span.rct-checkbox")
 
-    check_box_button = (By.CSS_SELECTOR, "button.rct-collapse.rct-collapse-btn")
-    def click_check_box(self):
-        self.click(self.check_box_button)
+    result_text = (By.ID, "result")
 
-    ch_b_desktop_button = (By.XPATH, "(//button[@aria-label='Toggle'])[2]")
-    def click_check_box_d_b(self):
-        self.click(self.ch_b_desktop_button)
+    def __init__(self, driver):
+        super().__init__(driver)
 
-    ch_b_documents_button = (By.XPATH, "(//button[@aria-label='Toggle'])[3]")
-    def click_check_box_doc_button(self):
-        self.click(self.ch_b_documents_button)
+    def toggle_node(self, node_name):
+        """Berilgan tugunni ochish yoki yopish uchun toggle tugmasini bosadi."""
+        locator = (By.XPATH, f"//label[@for='tree-node-{node_name}']//button[@aria-label='Toggle']")
+        self.click(locator)
 
-    ch_b_downloads_button = (By.XPATH, "(//button[@aria-label='Toggle'])[4]")
-    def click_check_box_download_button(self):
-        self.click(self.ch_b_downloads_button)
+    def select_checkbox(self, checkbox_name):
+        """Berilgan nomdagi checkboxni tanlaydi yoki bekor qiladi."""
+        locator = (By.XPATH, f"//label[@for='tree-node-{checkbox_name}']")
+        self.click(locator)
 
-    ch_b_button_one = (By.XPATH, "//label[@for='tree-node-desktop']")
-    def click_button_one(self):
-        self.click(self.ch_b_button_one)
+    def get_selected_items_text(self):
+        """Natija qutisidagi tanlangan elementlarning matnini oladi."""
+        result_locator = (By.ID, "result")
+        return self.get_text(result_locator)
 
-    ch_b_button_two = (By.XPATH, "//label[@for='tree-node-documents']")
-    def click_button_two(self):
-        self.click(self.ch_b_button_two)
+# OTHER WAYS - CLICK EVERYTHING
+#
+# from selenium.webdriver.common.by import By
+# from base.base_page import BasePage
+#
+# class Check_box_page(BasePage):
+#
+#     check_box_button = (By.CSS_SELECTOR, "button.rct-collapse.rct-collapse-btn")
+#     def click_check_box(self):
+#         self.click(self.check_box_button)
+#
+#     ch_b_desktop_button = (By.XPATH, "(//button[@aria-label='Toggle'])[2]")
+#     def click_check_box_d_b(self):
+#         self.click(self.ch_b_desktop_button)
+#
+#     ch_b_documents_button = (By.XPATH, "(//button[@aria-label='Toggle'])[3]")
+#     def click_check_box_doc_button(self):
+#         self.click(self.ch_b_documents_button)
+#
+#     ch_b_downloads_button = (By.XPATH, "(//button[@aria-label='Toggle'])[4]")
+#     def click_check_box_download_button(self):
+#         self.click(self.ch_b_downloads_button)
+#
+#     ch_b_button_one = (By.XPATH, "//label[@for='tree-node-desktop']")
+#     def click_button_one(self):
+#         self.click(self.ch_b_button_one)
+#
+#     ch_b_button_two = (By.XPATH, "//label[@for='tree-node-documents']")
+#     def click_button_two(self):
+#         self.click(self.ch_b_button_two)
+#
+#     ch_b_button_three = (By.XPATH, "//label[@for='tree-node-downloads']")
+#     def click_button_three(self):
+#         self.click(self.ch_b_button_three)
+#
 
-    ch_b_button_three = (By.XPATH, "//label[@for='tree-node-downloads']")
-    def click_button_three(self):
-        self.click(self.ch_b_button_three)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# class Check_box_page(BasePage):
+#
+#     check_box_button = (By.CSS_SELECTOR, "button.rct-collapse.rct-collapse-btn")
+#     def click_check_box(self):
+#         self.click(self.check_box_button)
+#
+#     ch_b_desktop_button = (By.XPATH, "(//button[@aria-label='Toggle'])[2]")
+#     def click_check_box_d_b(self):
+#         self.click(self.ch_b_desktop_button)
+#
+#     ch_b_documents_button = (By.XPATH, "(//button[@aria-label='Toggle'])[3]")
+#     def click_check_box_doc_button(self):
+#         self.click(self.ch_b_documents_button)
+#
+#     ch_b_downloads_button = (By.XPATH, "(//button[@aria-label='Toggle'])[4]")
+#     def click_check_box_download_button(self):
+#         self.click(self.ch_b_downloads_button)
+#
+#     ch_b_button_one = (By.XPATH, "//label[@for='tree-node-desktop']")
+#     def click_button_one(self):
+#         self.click(self.ch_b_button_one)
+#
+#     ch_b_button_two = (By.XPATH, "//label[@for='tree-node-documents']")
+#     def click_button_two(self):
+#         self.click(self.ch_b_button_two)
+#
+#     ch_b_button_three = (By.XPATH, "//label[@for='tree-node-downloads']")
+#     def click_button_three(self):
+#         self.click(self.ch_b_button_three)
+#
 
--- tests/test_check_box.py
+++ tests/test_check_box.py
@@ -1,16 +1,117 @@
 from base.base_page import BasePage
 from pages.check_box import Check_box_page
-from utils.driver import get_driver
+import time
 
-def test_check_box():
-    driver = get_driver("https://demoqa.com/checkbox")
-    base_page =BasePage(driver)
+
+def test_select_home_checkbox(driver):
+    """
+    'Home' checkboxini tanlash va natijani tekshirish testi.
+    'Home' ni tanlash barcha ichki elementlarni ham tanlaydi.
+    """
+    driver.get("https://demoqa.com/checkbox")
     check_box_page = Check_box_page(driver)
 
-    check_box_page.click_check_box()
-    check_box_page.click_check_box_d_b()
-    check_box_page.click_check_box_doc_button()
-    check_box_page.click_check_box_download_button()
-    check_box_page.click_button_one()
-    check_box_page.click_button_two()
-    check_box_page.click_button_three()
+    # 'Home' checkboxini tanlash
+    check_box_page.select_checkbox("home")
+
+    # Natija matnini olish va tekshirish
+    result_text = check_box_page.get_selected_items_text()
+
+    assert "home" in result_text
+    assert "desktop" in result_text
+    assert "documents" in result_text
+    assert "downloads" in result_text
+
+    time.sleep(1)
+
+
+def test_select_desktop_checkbox(driver):
+    """
+    'Desktop' checkboxini tanlash va natijani tekshirish testi.
+    Buning uchun avval papka ochilishi kerak.
+    """
+    driver.get("https://demoqa.com/checkbox")
+    check_box_page = Check_box_page(driver)
+
+    # 'Home' papkasini ochish
+    check_box_page.click(check_box_page.main_toggle_button)
+
+    # 'Desktop' checkboxini tanlash
+    check_box_page.select_checkbox("desktop")
+
+    # Natija matnini olish va tekshirish
+    result_text = check_box_page.get_selected_items_text()
+
+    assert "desktop" in result_text
+    assert "notes" in result_text
+    assert "commands" in result_text
+    assert "documents" not in result_text  # Bu element tanlanmaganini tekshiramiz
+
+    time.sleep(1)
+
+
+def test_select_documents_checkbox(driver):
+    """
+    'Documents' checkboxini tanlash va natijani tekshirish testi.
+    """
+    driver.get("https://demoqa.com/checkbox")
+    check_box_page = Check_box_page(driver)
+
+    # 'Home' papkasini ochish
+    check_box_page.click(check_box_page.main_toggle_button)
+
+    # 'Documents' checkboxini tanlash
+    check_box_page.select_checkbox("documents")
+
+    # Natija matnini olish va tekshirish
+    result_text = check_box_page.get_selected_items_text()
+
+    assert "documents" in result_text
+    assert "workspace" in result_text
+    assert "react" in result_text
+    assert "desktop" not in result_text  # Bu element tanlanmaganini tekshiramiz
+
+    time.sleep(1)
+
+
+def test_select_downloads_checkbox(driver):
+    """
+    'Downloads' checkboxini tanlash va natijani tekshirish testi.
+    """
+    driver.get("https://demoqa.com/checkbox")
+    check_box_page = Check_box_page(driver)
+
+    # 'Home' papkasini ochish
+    check_box_page.click(check_box_page.main_toggle_button)
+
+    # 'Downloads' checkboxini tanlash
+    check_box_page.select_checkbox("downloads")
+
+    # Natija matnini olish va tekshirish
+    result_text = check_box_page.get_selected_items_text()
+
+    assert "downloads" in result_text
+    assert "wordFile" in result_text
+    assert "excelFile" in result_text
+    assert "documents" not in result_text  # Bu element tanlanmaganini tekshiramiz
+
+    time.sleep(1)
+
+
+#OTHER WAYS - CLICK EVERYTHING
+#from base.base_page import BasePage
+# from pages.check_box import Check_box_page
+# from utils.driver import get_driver
+#
+# def test_check_box():
+#     driver = get_driver("https://demoqa.com/checkbox")
+#     base_page =BasePage(driver)
+#     check_box_page = Check_box_page(driver)
+#
+#     check_box_page.click_check_box()
+#     check_box_page.click_check_box_d_b()
+#     check_box_page.click_check_box_doc_button()
+#     check_box_page.click_check_box_download_button()
+#     check_box_page.click_button_one()
+#     check_box_page.click_button_two()
+#     check_box_page.click_button_three()
